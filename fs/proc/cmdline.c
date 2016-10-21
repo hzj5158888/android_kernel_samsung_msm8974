366dffe6a17dfa7c20ada1ef292ff9cf82ff2c97
Sultanxda <sultanxda@gmail.com>
2016-10-21 01:37:16 -0700

    proc: Remove verifiedbootstate flag from /proc/cmdline
    Userspace parses this and sets the ro.boot.verifiedbootstate prop
    according to the value that this flag has. When ro.boot.verifiedbootstate
    is not 'green', SafetyNet is tripped and fails the CTS test.
    
    Hide verifiedbootstate from /proc/cmdline in order to fix the failed
    SafetyNet CTS check.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Conflicts:
    	fs/proc/cmdline.c

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,15 +3,18 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/module.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 	char *temp_saved_command_line, *temp;
 	int i;
 	
-	temp_saved_command_line = kmalloc(strlen(saved_command_line)+1, GFP_KERNEL);
-	memcpy(temp_saved_command_line, saved_command_line, strlen(saved_command_line)+1);
-	temp=strstr(temp_saved_command_line, "array");
+	temp_new_command_line = kmalloc(strlen(new_command_line)+1, GFP_KERNEL);
+	memcpy(temp_new_command_line, new_command_line, strlen(new_command_line)+1);
+	temp=strstr(temp_new_command_line, "array");
 
 	if (temp!=NULL) {
 		for(i=0;i<20;i++)	*(temp+i)='*';
@@ -19,7 +22,7 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 
 	seq_printf(m, "%s\n", temp_saved_command_line);
 
-	kfree(temp_saved_command_line);
+	kfree(temp_new_command_line);
 	return 0;
 }
 
@@ -37,6 +40,29 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+	char *offset_addr, *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	/*
+	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
+	 * by userspace in order to pass SafetyNet CTS check.
+	 */
+	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
+	if (offset_addr) {
+		size_t i, len, offset;
+
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+	}
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
